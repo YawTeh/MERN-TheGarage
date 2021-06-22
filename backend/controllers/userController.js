8ee5828b72061e60254828a10eff00f88f8c40ec
Yaw Teh <yawteh@gmail.com>
2021-06-22 14:29:43 -0400

    Created Authentication Middleware for Protected Routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,11 @@
 import asyncHandler from "express-async-handler";
-import generateToken from "../utils/generateToken.js"
+import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
 const authUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body
+    const { email, password } = req.body;
 
-    const user = await User.findOne({ email })
+    const user = await User.findOne({ email });
 
     if (user && (await user.matchPassword(password))) {
         res.json({
@@ -14,11 +14,27 @@ const authUser = asyncHandler(async (req, res) => {
             email: user.email,
             isAdmin: user.isAdmin,
             token: generateToken(user._id),
-        })
+        });
     } else {
-        res.status(401)
-        throw new Error ("Invalid Email or Password")
+        res.status(401);
+        throw new Error("Invalid Email or Password");
     }
 });
 
-export { authUser }
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(404);
+        throw new Error("User Not Found");
+    }
+});
+
+export { authUser, getUserProfile };
