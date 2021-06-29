5ce1b73ef703bc0f214d791de50d00b969eeef96
Yaw Teh <yawteh@gmail.com>
2021-06-29 17:23:59 -0400

    Created Order Controller and Route

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-
+import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 
@@ -13,7 +13,7 @@ connectDB();
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("API is running...");
@@ -21,6 +21,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.use(notFound);
 
