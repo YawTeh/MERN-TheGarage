7f8378970879d4c8dc732e9682280be589f812e5
Yaw Teh <yawteh@gmail.com>
2021-07-01 00:48:19 -0400

    Created Admin Permissions

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -11,16 +11,16 @@ const protect = asyncHandler(async (req, res, next) => {
     ) {
         try {
             token = req.headers.authorization.split(" ")[1];
-            
+
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
             req.user = await User.findById(decoded.id).select("-password");
 
             next();
         } catch (error) {
-            console.error(error)
-            res.status(401)
-            throw new Error ("Not Authorized, Token Failed")
+            console.error(error);
+            res.status(401);
+            throw new Error("Not Authorized, Token Failed");
         }
     }
     if (!token) {
@@ -29,4 +29,13 @@ const protect = asyncHandler(async (req, res, next) => {
     }
 });
 
-export { protect };
+const admin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        next();
+    } else {
+        res.status(401);
+        throw new Error("Not Authorized as Admin");
+    }
+};
+
+export { protect, admin };
