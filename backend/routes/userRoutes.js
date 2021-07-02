0d6255816a97b3d4da8679c9ca933fb0f0c0bb2a
Yaw Teh <yawteh@gmail.com>
2021-07-01 23:33:11 -0400

    Created Admin Delete User Functionality

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,6 +5,7 @@ import {
     registerUser,
     updateUserProfile,
     getUsers,
+    deleteUser,
 } from "../controllers/userController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
@@ -13,8 +14,10 @@ const router = express.Router();
 router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/login", authUser);
 router
-    .route('/profile')
+    .route("/profile")
     .get(protect, getUserProfile)
     .put(protect, updateUserProfile);
 
+router.route("/:id").delete(protect, admin, deleteUser);
+ 
 export default router;
